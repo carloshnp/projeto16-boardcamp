ed0447d42b471ad1258fbef652170903e4d552aa
carloshnp <carloshnpwork@gmail.com>
2022-12-12 13:17:17 -0300

    refact: variables name

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -2,8 +2,8 @@ import { connection } from "../database/db.js";
 
 export async function getCategories(req, res) {
   try {
-    const categories = await connection.query("SELECT * FROM categories;");
-    res.send(categories.rows);
+    const query = await connection.query("SELECT * FROM categories;");
+    res.send(query.rows);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
@@ -12,26 +12,23 @@ export async function getCategories(req, res) {
 
 export async function insertCategory(req, res) {
   const { name } = req.body;
-
   if (name.length === 0) {
-    res.sendStatus(400);
-    return;
+    return res.sendStatus(400);
   }
-
   try {
     const catExists = await connection.query(
       `SELECT * FROM categories WHERE name = $1;`,
       [name]
     );
     if (catExists.rows.length > 0) {
-      res.sendStatus(409);
-      return;
+      return res.sendStatus(409);
     }
-    const createCat = await connection.query(
+
+    const query = await connection.query(
       `INSERT INTO categories (name) VALUES ($1)`,
       [name]
     );
-    console.log(createCat);
+    console.log(query);
     res.sendStatus(201);
   } catch (error) {
     console.log(error);
