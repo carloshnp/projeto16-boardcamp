2c71b1618ecf9fdb64f21c8c9cb3d1227e94767c
carloshnp <carloshnpwork@gmail.com>
2022-12-13 14:36:53 -0300

    feat: post rentals middlewares + addRental

--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getRentals } from "../controllers/rentalsController.js";
+import { addRental, getRentals } from "../controllers/rentalsController.js";
+import { isCustomer, isGame, isInStock } from "../middlewares/rentalsMiddleware.js";
 
 const router = Router();
 
 router.get('/rentals', getRentals)
-router.post('/rentals')
+router.post('/rentals', isCustomer, isGame, isInStock, addRental)
 router.post('/rentals/:id/return')
 router.delete('/rentals/:id')
 
