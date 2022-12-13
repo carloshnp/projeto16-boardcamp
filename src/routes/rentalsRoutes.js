bf55f24a33167dbe845174652115975a854083bb
carloshnp <carloshnpwork@gmail.com>
2022-12-13 15:21:16 -0300

    feat: finishRental post route + isRental, isReturned middlewares

--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { addRental, getRentals } from "../controllers/rentalsController.js";
-import { isCustomer, isGame, isInStock } from "../middlewares/rentalsMiddleware.js";
+import { addRental, finishRental, getRentals } from "../controllers/rentalsController.js";
+import { isCustomer, isGame, isInStock, isRental, isReturned } from "../middlewares/rentalsMiddleware.js";
 
 const router = Router();
 
 router.get('/rentals', getRentals)
 router.post('/rentals', isCustomer, isGame, isInStock, addRental)
-router.post('/rentals/:id/return')
+router.post('/rentals/:id/return', isRental, isReturned, finishRental)
 router.delete('/rentals/:id')
 
 export default router;
