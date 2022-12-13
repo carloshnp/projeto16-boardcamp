182965cd2696992e87b9117e859833c4070b7c97
carloshnp <carloshnpwork@gmail.com>
2022-12-13 14:37:23 -0300

    feat: isCustomer, isGame, isInStock

--- src/middlewares/rentalsMiddleware.js
+++ src/middlewares/rentalsMiddleware.js
@@ -1,13 +1,48 @@
 import { connection } from "../database/db.js";
 
 export async function isCustomer(req, res, next) {
+  try {
+    const { customerId } = req.body;
+    const query = await connection.query(
+      `SELECT * FROM customers WHERE id=$1;`,
+      [customerId]
+    );
+    if (!query.rowCount) {
+      return res.sendStatus(400);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function isGame(req, res, next) {
+  try {
+    const { gameId } = req.body;
+    const query = await connection.query(
+        `SELECT * FROM games WHERE id=$1;`,
+        [gameId]
+    );
+    if (!query.rowCount) {
+      return res.sendStatus(400);
+    }
+    res.locals.game = query.rows[0];
+    next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function isInStock (req,res,next) {
     try {
-        const { customerId } = req.body;
+        const {id} = req.params
         const query = await connection.query(
-            `SELECT * FROM customers WHERE id=$1;`,
-            [customerId]
+            `SELECT COUNT(*) FROM rentals WHERE "gameId"=$1`,
+            [id]
         )
-        if (!query.rowCount) {
+        if (res.locals.game.stockTotal - query <= 0) {
             return res.sendStatus(400)
         }
         next();
