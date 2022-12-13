f545830089a58e62b732473a8b4fdbd10c9074f9
carloshnp <carloshnpwork@gmail.com>
2022-12-13 12:54:43 -0300

    feat: customersRouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import categoriesRouter from "./routes/categoriesRoutes.js";
 import gamesRouter from "./routes/gamesRoutes.js";
+import customersRouter from './routes/customersRoutes.js'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.use(categoriesRouter);
 app.use(gamesRouter);
+app.use(customersRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("Server running in port: " + port));
