39f489a16ca2535d4aabfb15e6e9b03ec49c7682
carloshnp <carloshnpwork@gmail.com>
2022-12-13 16:43:40 -0300

    feat: get rentals joined with game + customer tables

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -6,20 +6,85 @@ export async function getRentals(req, res) {
     const { customerId, gameId } = req.query;
     if (customerId) {
       const query = await connection.query(
-        `SELECT * FROM rentals WHERE "customerId"=$1;`,
+        `SELECT rentals.*, customers.id AS "customer.id", customers.name AS "customer.name", games.id AS "game.id", games.name AS "game.name", games."categoryId" AS "game.categoryId", categories.name AS "game.categoryName" FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id WHERE "customerId"=$1;`,
         [customerId]
       );
-      return res.send(query.rows);
+      return res.send(
+        query.rows.map((cell) => ({
+          id: cell.id,
+          customerId: cell.customerId,
+          gameId: cell.gameId,
+          rentDate: cell.rentDate,
+          daysRented: cell.daysRented,
+          returnDate: cell.returnDate,
+          originalPrice: cell.originalPrice,
+          delayFee: cell.delayFee,
+          customer: {
+            id: cell["customer.id"],
+            name: cell["customer.name"],
+          },
+          game: {
+            id: cell["game.id"],
+            name: cell["game.name"],
+            categoryId: cell["game.categoryId"],
+            categoryName: cell["game.categoryName"],
+          },
+        }))
+      );
     }
     if (gameId) {
       const query = await connection.query(
-        `SELECT * FROM rentals WHERE "gameId"=$1`,
+        `SELECT rentals.*, customers.id AS "customer.id", customers.name AS "customer.name", games.id AS "game.id", games.name AS "game.name", games."categoryId" AS "game.categoryId", categories.name AS "game.categoryName" FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id WHERE "gameId"=$1`,
         [gameId]
       );
-      return res.send(query.rows);
+      return res.send(
+        query.rows.map((cell) => ({
+          id: cell.id,
+          customerId: cell.customerId,
+          gameId: cell.gameId,
+          rentDate: cell.rentDate,
+          daysRented: cell.daysRented,
+          returnDate: cell.returnDate,
+          originalPrice: cell.originalPrice,
+          delayFee: cell.delayFee,
+          customer: {
+            id: cell["customer.id"],
+            name: cell["customer.name"],
+          },
+          game: {
+            id: cell["game.id"],
+            name: cell["game.name"],
+            categoryId: cell["game.categoryId"],
+            categoryName: cell["game.categoryName"],
+          },
+        }))
+      );
     }
-    const query = await connection.query("SELECT * FROM rentals");
-    res.send(query.rows);
+    const query = await connection.query(
+      `SELECT rentals.*, customers.id AS "customer.id", customers.name AS "customer.name", games.id AS "game.id", games.name AS "game.name", games."categoryId" AS "game.categoryId", categories.name AS "game.categoryName" FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id;`
+    );
+    res.send(
+      query.rows.map((cell) => ({
+        id: cell.id,
+        customerId: cell.customerId,
+        gameId: cell.gameId,
+        rentDate: cell.rentDate,
+        daysRented: cell.daysRented,
+        returnDate: cell.returnDate,
+        originalPrice: cell.originalPrice,
+        delayFee: cell.delayFee,
+        customer: {
+          id: cell["customer.id"],
+          name: cell["customer.name"],
+        },
+        game: {
+          id: cell["game.id"],
+          name: cell["game.name"],
+          categoryId: cell["game.categoryId"],
+          categoryName: cell["game.categoryName"],
+        },
+      }))
+    );
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
@@ -30,7 +95,7 @@ export async function addRental(req, res) {
   try {
     const { customerId, gameId, daysRented } = req.body;
     if (daysRented <= 0) {
-        return res.sendStatus(400)
+      return res.sendStatus(400);
     }
     await connection.query(
       `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, (SELECT CURRENT_DATE), $3, null, (SELECT "pricePerDay" FROM games where id=$2)*$3, null);`,
