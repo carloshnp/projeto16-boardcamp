b73742bf5321d1ab22947c75603f079d2c9d7dba
carloshnp <carloshnpwork@gmail.com>
2022-12-13 14:06:34 -0300

    feat: use rentals router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import categoriesRouter from "./routes/categoriesRoutes.js";
 import gamesRouter from "./routes/gamesRoutes.js";
-import customersRouter from './routes/customersRoutes.js'
+import customersRouter from "./routes/customersRoutes.js";
+import rentalsRouter from "./routes/rentalsRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(categoriesRouter);
 app.use(gamesRouter);
 app.use(customersRouter);
+app.use(rentalsRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("Server running in port: " + port));
